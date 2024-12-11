09a6833d868c4f84aa3c76a1dc61c06a010a7745
vishnu-wnc <vishnu.chauhan@wakencode.com>
2024-12-11 18:18:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 function myFunction() {
-    var name = document.getElementById("first-name").value.toString();
+    var fname = document.getElementById("first-name").value.toString();
     var familyName = document.getElementById("family-name").value.toString();
     var username = document.getElementById("username").value.toString();
     var gender = document.getElementById("gender-select").value.toString();
 
     const data = {
-        "name": name,
+        "name": fname,
         "familyName": familyName,
         "gender": gender,
         "relations": [],
     }
-
     console.log(data);
-
-    localStorage.setItem(username, JSON.stringify(data));
-    localStorage.setItem("default", username);
+    if(localStorage.getItem(username) == null){
+        localStorage.setItem(username, JSON.stringify(data));
+        localStorage.setItem("default", username);
+    }
+    else{
+        mainData = JSON.parse(localStorage.getItem(username));
+        data.relations = mainData.relations;
+        localStorage.setItem(username, null);
+        localStorage.setItem(username, JSON.stringify(data));
+        localStorage.setItem("default", username);
+    }
 }
